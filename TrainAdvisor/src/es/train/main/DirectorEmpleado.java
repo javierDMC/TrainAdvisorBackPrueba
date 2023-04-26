0a43b28b21f02ba1786e223a5d67546ab31161cc
javierDMC <javierdelmoralcamacho@gmail.com>
2023-04-26 10:13:29 +0200

    Ciclo de vida del Bean

--- TrainAdvisor/src/es/train/main/DirectorEmpleado.java
+++ TrainAdvisor/src/es/train/main/DirectorEmpleado.java
@@ -51,5 +51,22 @@ public class DirectorEmpleado implements Empleados {
 		// TODO Auto-generated method stub
 		return "Informe creado por el director: " + informeNuevo.getInforme();
 	}
+	
+	
+	// metodo init. Ejecutar tareas antes de que el bean esté disponible
+	
+	public void metodoIncial() {
+		
+		System.out.println("Dentro del metodo init. Aqui irian las tareas a ejecutar antes de que el bean este listo");
+	}
+	
+	
+	// metodo destroy. Ejecutar tareas despues de que el bean haya sido utilizado
+	
+	public void metodoFinal() {
+		
+		System.out.println("Dentro del metodo destroy. Aqui irian las tareas a ejecutar despues de utilizar el bean");
+	}
+	
 
 }
